30edbb6902574d24a84ed18cbc02e573b522f998
earl-stephens <stephensearl@hotmail.com>
2022-01-27 23:23:57 -0700

    Demonstrate method overloading

--- src/application/App.java
+++ src/application/App.java
@@ -3,7 +3,20 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Person person = new Person();
+		
+		person.greet();
+		
+		person.greet("Joe");
+		
+		person.greet(186);
+		
+		person.greet("Sally", 160);
+		
+		/* This is method overloading.  The methods differ in the
+		 * arguments that are supplied.  Java picks the method that 
+		 * uses the same data type of argument.
+		 */
 
 	}
 
